d4c87883c6180d74322c3e79f73dc5727090b64d
Luis Izquierdo L <luizquierdo@gmail.com>
2019-11-06 18:05:34 -0300

    Agregando todos los primeros archivos

---
+++ bukerp.py
@@ -0,0 +1,16 @@
+from frappeclient import FrappeClient
+import json
+
+
+try:
+    client = FrappeClient("https://erp.tecton.cl", "lizquierdo@tecton.cl", "tecton")
+except:
+    print("ERROR LOGEARSE AL ERP")
+    raise
+
+with open("voucher_template.json", "r") as read_file:
+    voucher = json.load(read_file)
+
+resultado = client.insert(voucher)
+
+print(resultado)
