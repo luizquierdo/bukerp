ed58a090dd42a6b75522a85601f855c72801351d
Luis Izquierdo L <luizquierdo@gmail.com>
2019-11-06 19:05:34 -0300

    Agregando todos los primeros archivos

--- buk.py
+++ buk.py
@@ -1,7 +1,10 @@
 import requests
+import json
 
 headers = {'content-type': 'application/json', 'auth_token': 'daSY92P7JMXZzFBkZCDDMYiU'}
 url = 'https://tecton.buk.cl/api/v1/accounting/export?month=10&year=2019'
 r = requests.get(url, headers=headers)
 
 print(r.content)
+buk = json.loads(r.content)
+print(buk["data"]["76.407.152-2"]["Constructora Tecton S.p.A."])
