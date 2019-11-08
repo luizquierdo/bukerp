3f60f6bd71f35eb935c43df3fe31e41eaa77e952
Luis Izquierdo L <luizquierdo@gmail.com>
2019-11-08 13:28:04 -0300

    Agregando todos los primeros archivos

--- buk.py
+++ buk.py
@@ -1,10 +1,21 @@
 import requests
 import json
+import pandas as pd
+from pandas.io.json import json_normalize
+import numpy as np
 
 headers = {'content-type': 'application/json', 'auth_token': 'daSY92P7JMXZzFBkZCDDMYiU'}
-url = 'https://tecton.buk.cl/api/v1/accounting/export?month=10&year=2019'
+url = 'https://tecton.buk.cl/api/v1/accounting/export?month=10&year=2019&company_id=76.407.152-2'
 r = requests.get(url, headers=headers)
 
-print(r.content)
+
 buk = json.loads(r.content)
-print(buk["data"]["76.407.152-2"]["Constructora Tecton S.p.A."])
+
+data = json_normalize(buk["data"]["76.407.152-2"]["Constructora Tecton S.p.A."])
+
+data["deber"] = data["deber"].replace({'': 0})
+data["haber"] = data["haber"].replace({'': 0})
+
+grouped = data.groupby("centro_costo")
+
+print(grouped["deber", "haber"].agg(np.sum))
