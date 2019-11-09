caa7b16dd9bf54461200e87335e0a135d535e471
Luis Izquierdo L <luizquierdo@gmail.com>
2019-11-09 12:22:38 -0300

    Lectura y agrupamiento de datos de BUK OK

--- buk.py
+++ buk.py
@@ -2,6 +2,7 @@ import requests
 import json
 from pandas.io.json import json_normalize
 import numpy as np
+import pandas as pd
 
 headers = {'content-type': 'application/json', 'auth_token': 'daSY92P7JMXZzFBkZCDDMYiU'}
 url = 'https://tecton.buk.cl/api/v1/accounting/export?month=10&year=2019&company_id=76.407.152-2'
@@ -19,6 +20,17 @@ with open("relaciones.json", "r") as read_file:
 
 data["cuenta_contable"] = data["cuenta_contable"].replace(relaciones)
 
-grouped = data.groupby(["centro_costo", "cuenta_contable"])
+data_buk_agrupada = data.groupby(["centro_costo", "cuenta_contable"])["deber", "haber"].agg(np.sum)
 
-print(grouped["deber", "haber"].agg(np.sum))
+#imprimir los distintos centros de costo
+print(data.centro_costo.unique())
+
+#for(centro_costo in data.centro_costo.unique()):
+
+
+print(data_buk_agrupada.loc['2014.OFI.001'])
+
+#for index, row in data_buk_agrupada.iterrows():
+#    print(index[0], index[1], row['deber'], row['haber'])
+
+
